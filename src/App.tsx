087466b161bd9db6a90e41d6258621ed70501df4
Lucas <92234503+lucaspalomo93@users.noreply.github.com>
2025-01-04 15:58:49 -0300

    Added useCallback and useMemo examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UseRefExample3 from './components/UseRefExample3';
+import UseCallBackExample from './components/UseCallBackExample';
+// import UseMemoExample from './components/UseMemoExample';
+// import UseRefExample3 from './components/UseRefExample3';
 // import UseRefExample1 from './components/UseRefExample1';
 // import UseRefExmaple2 from './components/UseRefExmaple2';
 
 function App() {
   return (
     <>
+      <h1 className='bg-slate-200 text-black bold font-bold text-center py-3 mb-10'>
+        HOOKS
+      </h1>
       {/* <UseRefExample1 /> */}
       {/* <UseRefExmaple2 /> */}
-      <UseRefExample3 />
+      {/* <UseRefExample3 /> */}
+      {/* <UseMemoExample /> */}
+      <UseCallBackExample />
     </>
   );
 }
