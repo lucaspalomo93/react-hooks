b76bebcf4315791041d9d07d28cb70c38259e2ea
Lucas <92234503+lucaspalomo93@users.noreply.github.com>
2025-01-04 16:44:08 -0300

    Added task interface

--- src/components/CustomHookExample2.tsx
+++ src/components/CustomHookExample2.tsx
@@ -1,6 +1,12 @@
 import { FormEvent } from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 
+interface Task {
+	task: string;
+	completed: boolean;
+	date: string;
+}
+
 const CustomHookExample2 = () => {
 	const [task, setTask] = useLocalStorage('task', '');
 	const [tasks, setTasks] = useLocalStorage('tasks', []);
@@ -17,20 +23,25 @@ const CustomHookExample2 = () => {
 	};
 
 	return (
-		<form onSubmit={onSubmit} className='w-50 ml-6'>
-			<div className='mb-3'>
-				<label className='form-label'>Task</label>
-				<input
-					className='form-control'
-					type='text'
-					value={task}
-					onChange={(e) => setTask(e.target.value)}
-				/>
-			</div>
-			<button className='btn btn-primary' type='submit'>
-				Submit
-			</button>
-		</form>
+		<>
+			<form onSubmit={onSubmit} className='w-50 ml-6'>
+				<div className='mb-3'>
+					<label className='form-label'>Task</label>
+					<input
+						className='form-control'
+						type='text'
+						value={task}
+						onChange={(e) => setTask(e.target.value)}
+					/>
+				</div>
+				<button className='btn btn-primary' type='submit'>
+					Submit
+				</button>
+			</form>
+			{tasks.map((task: Task) => (
+				<h3 key={task.date}>{task.task}</h3>
+			))}
+		</>
 	);
 };
 
