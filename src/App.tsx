3fddd853e7f33cfe50b93d8851df92e0efe1bc47
Lucas <92234503+lucaspalomo93@users.noreply.github.com>
2025-01-04 15:07:52 -0300

    Added examples of useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UseRefExample1 from './components/UseRefExample1';
+import UseRefExample3 from './components/UseRefExample3';
+// import UseRefExample1 from './components/UseRefExample1';
+// import UseRefExmaple2 from './components/UseRefExmaple2';
 
 function App() {
   return (
     <>
-      <UseRefExample1 />
+      {/* <UseRefExample1 /> */}
+      {/* <UseRefExmaple2 /> */}
+      <UseRefExample3 />
     </>
   );
 }
